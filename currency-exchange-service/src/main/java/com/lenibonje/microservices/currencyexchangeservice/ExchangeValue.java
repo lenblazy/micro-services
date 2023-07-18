da1ce95cfe47c29f3ced04c66fc77fd97aed8cfc
lenblazy <lenblazy@gmail.com>
2023-07-18 10:38:29 +0300

    Configure JPA and initialized Data

--- currency-exchange-service/src/main/java/com/lenibonje/microservices/currencyexchangeservice/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/lenibonje/microservices/currencyexchangeservice/ExchangeValue.java
@@ -1,21 +1,27 @@
 package com.lenibonje.microservices.currencyexchangeservice;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
+@Entity
 public class ExchangeValue {
 
+    @Id
     private Long id;
 
+    @Column(name = "currency_from")
     private String from;
 
+    @Column(name = "currency_to")
     private String to;
 
     private BigDecimal conversionMultiple;
 
     private int port;
 
-    public ExchangeValue() {
-    }
+    public ExchangeValue() {}
 
     public ExchangeValue(Long id, String from, String to, BigDecimal conversionMultiple) {
         this.id = id;
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+Insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port) values(1001,'USD','KES',240,0)
+Insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port) values(1002,'USD','UGX',8400,0)
+Insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port) values(1003,'USD','TZH',4800,0)
