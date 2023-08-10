dd3cc79a9222a95eefc0db73b9af8906c621feda
lenblazy <lenblazy@gmail.com>
2023-08-10 11:25:41 +0300

    Finish rate limiter

--- currency-exchange-service/src/main/java/com/lenibonje/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/lenibonje/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,6 +1,8 @@
 package com.lenibonje.microservices.currencyexchangeservice;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +18,9 @@ public class CircuitBreakerController {
 
     @GetMapping("/sample-api")
 //    @Retry(name= "sample-api", fallbackMethod = "hardcodedResponse")
-    @CircuitBreaker(name= "sample-api", fallbackMethod = "hardcodedResponse")
+//    @CircuitBreaker(name= "sample-api", fallbackMethod = "hardcodedResponse")
+//    @RateLimiter(name = "default")
+    @Bulkhead(name = "sample-api")
     public String sampleAPI(){
         logger.info("Sample API call received");
         ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy", String.class);
